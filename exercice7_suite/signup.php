05db6ae217b81b086dadefbb15f67d5f780b10d9
Anthony Laplane <anthony.laplane@gmail.com>
2025-01-28 10:51:31 +0100

    ex7

---
+++ exercice7_suite/signup.php
@@ -0,0 +1,6 @@
+<?php
+session_start();
+require_once "templates/header.php" ?>
+<h1>Accueil</h1>
+<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Possimus ipsam repellendus aspernatur deserunt inventore cumque? Eaque, molestias totam vel provident atque assumenda cumque, magni cum neque odio laborum tempora architecto!</p>
+<?php require_once "templates/footer.php" ?>
